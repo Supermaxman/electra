099d1e832135ab35ee6f777c0675ed6ca78270ba
Max Weinzierl <maxwellweinzierl@gmail.com>
2020-08-28 03:19:57 -0500

    Added initial preprocessing for pubmed article sentences

--- preprocess_vocab.py
+++ preprocess_vocab.py
@@ -5,7 +5,7 @@ import random
 
 
 def main():
-  random.seed(0)
+  random.seed(1)
   parser = argparse.ArgumentParser(description=__doc__)
   parser.add_argument("--corpus-filelist-path", required=True,
                       help="Location of pre-training text files.")
