1a636fbbc5ad0a174f319f061cc4798e0e247d70
Max Weinzierl <maxwellweinzierl@gmail.com>
2020-08-28 02:05:08 -0500

    Added initial preprocessing for pubmed article sentences

---
+++ preprocess_files.py
@@ -0,0 +1,24 @@
+
+import argparse
+from tokenizers import BertWordPieceTokenizer
+from pathlib import Path
+from tqdm import tqdm
+
+def main():
+  parser = argparse.ArgumentParser(description=__doc__)
+  parser.add_argument("--corpus-dir", required=True,
+                      help="Location of pre-training text files.")
+  args = parser.parse_args()
+
+  count = 0
+  with open('files.txt', 'w') as f:
+    for path in tqdm(Path(args.corpus_dir).rglob("*.[tT][xX][tT]")):
+      if path.is_file():
+        f.write(str(path)+'\n')
+        count += 1
+
+  print(f'Nrof files: {count}')
+
+
+if __name__ == "__main__":
+  main()
